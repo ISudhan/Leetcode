b988ffad9d78758545c41f7c143ee4fcf0986c0b
Sudhan N <sudhann.23aid@kongu.edu>
2025-04-19 23:00:16 +0530

    Time: 6 ms (9.14%), Space: 44.8 MB (9.46%) - LeetHub

---
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        String maxOddBinary = "";
+        int countOfOne = -1;
+        for(char ch:s.toCharArray()){
+            if(Integer.parseInt(ch+"")%2 == 1){
+                countOfOne++;
+            }
+        }
+        for(int i = 0;i<s.length()-1;i++){
+            if(countOfOne>0){
+                maxOddBinary +="1";
+                countOfOne--;
+            }
+            else{
+                maxOddBinary += "0";
+            }
+        }
+        return maxOddBinary+"1";
+    }
+}
