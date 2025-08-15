a8b05027cfcc0b3ca84e9b5e4d742e0da23e1adf
Sudhan N <sudhann.23aid@kongu.edu>
2025-08-15 23:14:55 +0530

    Time: 0 ms (100%), Space: 40.2 MB (84.62%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,22 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        String s;
-        for(int i = 2;i<=n-2;i++){
-            s = "";
-            int num = n;
-            while(num != 0){
-                int rem = num%i;
-                s = rem + s;
-                num /=i;
-            }
-            int p=0,q=s.length()-1;
-            while(p<q){
-                if(s.charAt(p) != s.charAt(q))
-                return false;
-                p++;
-                q--;
-            }
-        }
-        return true;
+        return false;
     }
 }
