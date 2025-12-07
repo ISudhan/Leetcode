5402a9d7b5a4f9b10ee09043dee92d8964b3f672
Sudhan N <sudhann.23aid@kongu.edu>
2025-12-07 22:16:48 +0530

    Time: 45 ms (93.58%), Space: 55 MB (25.66%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+    let a;
+    let b;
+    let c = promise1.then(function (n1) {
+        a = n1;
+        return promise2;
+    }).then(function(n2){
+        return a+n2;
+    })
+    return new Promise(function(resolve,reject){resolve(c)});
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
